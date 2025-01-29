3e697ba53ef90fa9c85f7794ee4d936cd526f5c8
livia <livia@DESKTOP-L8IN7G7>
2025-01-29 15:19:37 -0500

    Add project files.

---
+++ energy drink consumption.cpp
@@ -0,0 +1,40 @@
+// energy drink consumption.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    int totalcustomers;
+    double energydrinkpercent= 0.15;
+    double citrusflavorpercent = 0.58;
+
+    cout << "Enter the total number of customers surveyed: ";
+    cin >> totalcustomers;
+
+    int customersbuyingenergydrinks = totalcustomers * energydrinkpercent;
+    int customersperferringcitrus = customersbuyingenergydrinks * citrusflavorpercent;
+
+    cout << fixed << setprecision(0);
+    cout << "Approximate number of customers who purchase energy drinks:"
+        << customersbuyingenergydrinks << endl;
+    cout << "Approximate number of customers who prefer citrus flavored energy drinks; "
+        << customersperferringcitrus << endl;
+
+    return 0;
+    
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
